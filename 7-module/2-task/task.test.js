40a8b4e89f7df220b9fb0eeb59e18bb3d5d29f0d
Konstantin <fandeluk.k@gmail.com>
2020-05-04 23:03:18 +0300

    new task

--- 7-module/2-task/task.test.js
+++ 7-module/2-task/task.test.js
@@ -1,6 +1,6 @@
-import Modal from './index.js';
+import Modal from "./index.js";
 
-fdescribe('7-module-2-task', () => {
+describe("7-module-2-task", () => {
   let modal;
 
   beforeEach(() => {
@@ -11,7 +11,7 @@ fdescribe('7-module-2-task', () => {
     modal.close();
   });
 
-  describe('метод open()', () => {
+  describe("метод open()", () => {
     beforeEach(() => {
       modal.open();
     });
@@ -20,33 +20,35 @@ fdescribe('7-module-2-task', () => {
       modal.close();
     });
 
-    it('должна создавать основной элемент модального окна', () => {
-      let modalElement = document.body.querySelector('.modal');
+    it("должна создавать основной элемент модального окна", () => {
+      let modalElement = document.body.querySelector(".modal");
 
       expect(modalElement).toBeTruthy();
     });
 
-    it('должна создавать прозрачный фон', () => {
-      let modalElement = document.body.querySelector('.modal .modal__overlay');
+    it("должна создавать прозрачный фон", () => {
+      let modalElement = document.body.querySelector(".modal .modal__overlay");
 
       expect(modalElement).toBeTruthy();
     });
 
     it('должна добавлять класс "is-modal-open" элементу body', () => {
-      let hasIsModalOpenClass = document.body.classList.contains('is-modal-open');
+      let hasIsModalOpenClass = document.body.classList.contains(
+        "is-modal-open"
+      );
 
       expect(hasIsModalOpenClass).toBe(true);
     });
   });
 
   describe(`метод setTitle('modal title')`, () => {
-    it('должен задавать заголовок модального окна', () => {
-      let title = 'Я главное модальное окно';
+    it("должен задавать заголовок модального окна", () => {
+      let title = "Я главное модальное окно";
 
       modal.setTitle(title);
       modal.open();
 
-      let titleElement = document.querySelector('.modal');
+      let titleElement = document.querySelector(".modal");
 
       expect(titleElement.textContent.trim()).toBe(title);
 
@@ -56,18 +58,18 @@ fdescribe('7-module-2-task', () => {
 
   describe(`метод setBody(node)`, () => {
     it('должен вставлять содержимое модального окна в элемента с классом "modal__body"', () => {
-      let innerTitleText = 'Внутренний заголовок содержимого';
-      let node = document.createElement('div');
-      node.className = 'test-node-class';
+      let innerTitleText = "Внутренний заголовок содержимого";
+      let node = document.createElement("div");
+      node.className = "test-node-class";
       node.innerHTML = `<h4>${innerTitleText}</h4>`;
 
       modal.setBody(node);
 
       modal.open();
 
-      let modalBody = document.querySelector('.modal .modal__body');
-      let nodeFromModal = modalBody.querySelector('.test-node-class');
-      let innerTitle = nodeFromModal && nodeFromModal.querySelector('h4');
+      let modalBody = document.querySelector(".modal .modal__body");
+      let nodeFromModal = modalBody.querySelector(".test-node-class");
+      let innerTitle = nodeFromModal && nodeFromModal.querySelector("h4");
 
       let hasInnerTitle = innerTitle.textContent === innerTitleText;
 
@@ -77,7 +79,7 @@ fdescribe('7-module-2-task', () => {
     });
   });
 
-  describe('закрытие', () => {
+  describe("закрытие", () => {
     let clickEvent;
     let escKeyDownEvent;
     let closeButton;
@@ -85,15 +87,18 @@ fdescribe('7-module-2-task', () => {
     beforeEach(() => {
       modal.open();
 
-      clickEvent = new MouseEvent('click', { bubbles: true });
-      escKeyDownEvent = new KeyboardEvent('keydown', { code: 'Escape', bubbles: true });
-      closeButton = document.body.querySelector('.modal .modal__close');
+      clickEvent = new MouseEvent("click", { bubbles: true });
+      escKeyDownEvent = new KeyboardEvent("keydown", {
+        code: "Escape",
+        bubbles: true,
+      });
+      closeButton = document.body.querySelector(".modal .modal__close");
     });
 
-    it('при вызове метода close(), вёрстка модального окна должна удаляться из body', () => {
+    it("при вызове метода close(), вёрстка модального окна должна удаляться из body", () => {
       modal.close();
 
-      let modalElement = document.querySelector('.modal');
+      let modalElement = document.querySelector(".modal");
 
       expect(modalElement).toBeNull();
     });
@@ -101,15 +106,17 @@ fdescribe('7-module-2-task', () => {
     it('при вызове метода close(), должен удаляться класс "is-modal-open" с элемента body', () => {
       modal.close();
 
-      let hasIsModalOpenClass = document.body.classList.contains('is-modal-open');
+      let hasIsModalOpenClass = document.body.classList.contains(
+        "is-modal-open"
+      );
 
       expect(hasIsModalOpenClass).toBe(false);
     });
 
-    it('при клике по [X], вёрстка модального окна должна удаляться из body', () => {
+    it("при клике по [X], вёрстка модального окна должна удаляться из body", () => {
       closeButton.dispatchEvent(clickEvent);
 
-      let modalElement = document.querySelector('.modal');
+      let modalElement = document.querySelector(".modal");
 
       expect(modalElement).toBeNull();
     });
@@ -117,15 +124,17 @@ fdescribe('7-module-2-task', () => {
     it('при клике по [X], должен удаляться класс "is-modal-open" с элемента body', () => {
       closeButton.dispatchEvent(clickEvent);
 
-      let hasIsModalOpenClass = document.body.classList.contains('is-modal-open');
+      let hasIsModalOpenClass = document.body.classList.contains(
+        "is-modal-open"
+      );
 
       expect(hasIsModalOpenClass).toBe(false);
     });
 
-    it('при нажатии клавиши ESC, вёрстка модального окна должна удаляться из body', () => {
+    it("при нажатии клавиши ESC, вёрстка модального окна должна удаляться из body", () => {
       document.body.dispatchEvent(escKeyDownEvent);
 
-      let modalElement = document.querySelector('.modal');
+      let modalElement = document.querySelector(".modal");
 
       expect(modalElement).toBeNull();
     });
@@ -133,10 +142,11 @@ fdescribe('7-module-2-task', () => {
     it('при нажатии клавиши ESC, должен удаляться класс "is-modal-open" с элемента body', () => {
       document.body.dispatchEvent(escKeyDownEvent);
 
-      let hasIsModalOpenClass = document.body.classList.contains('is-modal-open');
+      let hasIsModalOpenClass = document.body.classList.contains(
+        "is-modal-open"
+      );
 
       expect(hasIsModalOpenClass).toBe(false);
     });
-
   });
 });
