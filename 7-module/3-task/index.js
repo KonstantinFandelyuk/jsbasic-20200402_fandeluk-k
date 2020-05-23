06ffc80c3d3febbf34adf19b00fe41b5a6c36a2a
Konstantin <fandeluk.k@gmail.com>
2020-05-23 22:47:24 +0300

    7.3

--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -7,7 +7,7 @@ export default class StepSlider {
     slider.classList.add("slider");
     this.steps = steps;
     this.value = value;
-    this.slider = slider;
+    this.elem = slider;
     //
     let template = `
     <!--Ползунок слайдера с активным значением-->
@@ -27,16 +27,16 @@ export default class StepSlider {
       </div>
     `;
     this.container.append(this.slider);
-    this.slider.insertAdjacentHTML(`beforeend`, template);
+    this.elem.insertAdjacentHTML(`beforeend`, template);
     //
-    this.slider.addEventListener("click", (event) => this.StepSlider(event));
+    this.elem.addEventListener("click", (event) => this.StepSlider(event));
   }
   StepSlider() {
     // Для начала определим расстояние от начала элемента слайдера до места, на котором находился курсор в момент клика. Мы будем использовать координаты относительно окна. Возьмем координату по горизонтали (из свойства clientX объекта события) и вычтем из нее координату крайней левой точки слайдера, которую получим с помощью метода getBoundingClientRect():
-    let left = event.clientX - this.slider.getBoundingClientRect().left;
+    let left = event.clientX - this.elem.getBoundingClientRect().left;
 
     // В итоге мы получим расстояние в пикселях от начала слайдера до места клика. Но нам нужно выбрать значение слайдера из дипазона – 0, 1, 2, 3, 4. Поэтому рассчитаем относительное значение, взяв за основу ширину слайдера:
-    let leftRelative = left / this.slider.offsetWidth;
+    let leftRelative = left / this.elem.offsetWidth;
 
     // Полученное значение будет в диапазоне от 0 до 1, ведь клик был внутри слайдера.
     // Как вы помните, нам нужно получить конкретное значение слайдера (0, 1, 2, 3, 4). Для этого возьмем полученное значение (переменная leftRelative) и умножим его на количество сегментов:
@@ -52,11 +52,11 @@ export default class StepSlider {
     //
     let leftPercents = valuePercents; // Значение в процентах от 0 до 100
 
-    let thumb = this.slider.querySelector(".slider__thumb");
-    let progress = this.slider.querySelector(".slider__progress");
+    let thumb = this.elem.querySelector(".slider__thumb");
+    let progress = this.elem.querySelector(".slider__progress");
     let StepSliderElement = document.querySelector(".slider__steps");
     let span = StepSliderElement.querySelectorAll("span");
-    this.slider.querySelector(".slider__value").innerHTML = this.value;
+    this.elem.querySelector(".slider__value").innerHTML = this.value;
 
     span.forEach((item) => {
       item.classList.remove("slider__step-active");
@@ -70,7 +70,7 @@ export default class StepSlider {
       }
     }
 
-    this.slider.dispatchEvent(
+    this.elem.dispatchEvent(
       new CustomEvent("slider-change", {
         detail: this.value,
         bubbles: true,
