e57d703bfda1301006f2c25ea9c5b3d0f57b226a
Konstantin <fandeluk.k@gmail.com>
2020-05-29 17:49:43 +0300

    -1 задача

--- 7-module/4-task/index.js
+++ 7-module/4-task/index.js
@@ -1,90 +1,61 @@
+import createElement from "../../assets/lib/create-element.js";
+
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
-    //
-    let slider = document.createElement("div");
-    slider.classList.add("slider");
     this.steps = steps;
+    this.segments = steps - 1;
+    this.render();
+
+    this.addEventListeners();
+
+    this.setValue(value);
+  }
+
+  render() {
+    this.elem = createElement(`
+      <div class="slider">
+        <div class="slider__thumb">
+          <span class="slider__value"></span>
+        </div>
+        <div class="slider__progress"></div>
+        <div class="slider__steps">
+          ${"<span></span>".repeat(this.steps)}
+        </div>
+      </div>
+    `);
+  }
+
+  setValue(value) {
     this.value = value;
-    this.elem = slider;
-    //
-    let template = `
-    <!--Ползунок слайдера с активным значением-->
-      <div class="slider__thumb">
-      <span class="slider__value">0</span>
-      </div>
-    <!--Полоска слайдера-->
-      <div class="slider__progress"></div>
-    <!-- Шаги слайдера (вертикальные чёрточки) -->
-      <div class="slider__steps">
-      <!-- текущий выбранный шаг выделен этим классом -->
-      <span class="slider__step-active"></span>
-      </div>
-    `;
-    this.elem.insertAdjacentHTML(`beforeend`, template);
-    //
-    this.stepsSpan();
-    this.elem.addEventListener("pointerdown", (event) => this.mouseDown(event));
-    this.elem.addEventListener("click", (event) => this.switchSlide(event));
+
+    let valuePercents = (value / this.segments) * 100;
+
+    this.sub("thumb").style.left = `${valuePercents}%`;
+    this.sub("progress").style.width = `${valuePercents}%`;
+
+    this.sub("value").innerHTML = value;
+
+    if (this.sub("step-active")) {
+      this.sub("step-active").classList.remove("slider__step-active");
+    }
+
+    this.sub("steps").children[this.value].classList.add("slider__step-active");
   }
-  stepsSpan() {
-    let firstSpan = this.elem.querySelector(".slider__steps");
-    for (let i = 0; i < this.steps - 1; i++) {
-      let spanElement = document.createElement("span");
-      firstSpan.insertAdjacentElement("beforeend", spanElement);
-    }
+
+  addEventListeners() {
+    this.sub("thumb").ondragstart = () => false;
+
+    this.sub("thumb").onpointerdown = this.onPointerDown;
+
+    this.elem.onclick = this.onClick;
   }
-  mouseDown(event) {
-    let thumb = document.querySelector(".slider__thumb");
-    thumb.ondragstart = () => false;
-    event.preventDefault();
 
-    thumb.addEventListener("pointermove", mouseMove);
-    thumb.addEventListener("pointerup", mouseUp);
+  onClick = (event) => {
+    let newLeft =
+      (event.clientX - this.elem.getBoundingClientRect().left) /
+      this.elem.offsetWidth;
 
-    function mouseMove() {
-      // let left = event.clientX - this.elem.getBoundingClientRect().left; - Выдает ошибку , А this.elem получаю андефайн
-      console.log(this.elem);
-    }
-
-    function mouseUp() {
-      thumb.removeEventListener("pointerup", mouseUp);
-      thumb.removeEventListener("pointermove", mouseMove);
-    }
-    // document.addEventListener("mousemove", onMouseMove);
-  }
-  switchSlide() {
-    // Для начала определим расстояние от начала элемента слайдера до места, на котором находился курсор в момент клика. Мы будем использовать координаты относительно окна. Возьмем координату по горизонтали (из свойства clientX объекта события) и вычтем из нее координату крайней левой точки слайдера, которую получим с помощью метода getBoundingClientRect():
-    let left = event.clientX - this.elem.getBoundingClientRect().left;
-    // В итоге мы получим расстояние в пикселях от начала слайдера до места клика. Но нам нужно выбрать значение слайдера из дипазона – 0, 1, 2, 3, 4. Поэтому рассчитаем относительное значение, взяв за основу ширину слайдера:
-    let leftRelative = left / this.elem.offsetWidth;
-    // Полученное значение будет в диапазоне от 0 до 1, ведь клик был внутри слайдера.
-    // Как вы помните, нам нужно получить конкретное значение слайдера (0, 1, 2, 3, 4). Для этого возьмем полученное значение (переменная leftRelative) и умножим его на количество сегментов:
-    let segments = this.steps - 1;
-    let approximateValue = leftRelative * segments;
-    // В итоге вероятнее всего вы получите некое дробное значение, например: 1.2345, 3.442 или подобное. При этом оно не будет меньше 0 и не больше максимально возможного значения слайдера, для нашего примера – это 4. Чтобы получить конкретно значение, которое нужно задать слайдеру, округлим дробное значение по правилам математики:
-    this.value = Math.round(approximateValue);
-    // Его мы и будем использовать для отображения в слайдере, как это требуется выше.
-    // Чтобы получить значение в процентах для перемещения ползунка и закрашивания заполненной области, выполним обратное преобразование, разделив value на количество сегментов и умножив на 100:
-    let valuePercents = (this.value / segments) * 100;
-    //
-    let leftPercents = valuePercents; // Значение в процентах от 0 до 100
-
-    let thumb = this.elem.querySelector(".slider__thumb");
-    let progress = this.elem.querySelector(".slider__progress");
-    let StepSliderElement = document.querySelector(".slider__steps");
-    let span = StepSliderElement.querySelectorAll("span");
-    this.elem.querySelector(".slider__value").innerHTML = this.value;
-    span.forEach((item) => {
-      item.classList.remove("slider__step-active");
-    });
-
-    if (progress) {
-      if (this.value <= span.length - 1) {
-        span[this.value].classList.add("slider__step-active");
-        thumb.style.left = `${leftPercents}%`;
-        progress.style.width = `${leftPercents}%`;
-      }
-    }
+    this.setValue(Math.round(this.segments * newLeft));
 
     this.elem.dispatchEvent(
       new CustomEvent("slider-change", {
@@ -92,5 +63,73 @@ export default class StepSlider {
         bubbles: true,
       })
     );
+  };
+
+  onPointerDown = (event) => {
+    event.preventDefault();
+
+    this.elem.classList.add("slider_dragging");
+
+    document.addEventListener("pointermove", this.onPointerMove);
+    document.addEventListener("pointerup", this.onPointerUp);
+  };
+
+  onPointerMove = (event) => {
+    event.preventDefault();
+
+    let newLeft = this.calcLeftByEvent(event);
+
+    this.sub("thumb").style.left = `${newLeft * 100}%`;
+    this.sub("progress").style.width = `${newLeft * 100}%`;
+
+    // Show the nearest value
+    // First half of step is 1, et
+    // |-------|-------|-------|-------|
+    // | 1 /   2   /   3   /   4   / 5 |
+    this.value = Math.round(this.segments * newLeft);
+    this.sub("value").innerHTML = this.value;
+
+    if (this.sub("step-active")) {
+      this.sub("step-active").classList.remove("slider__step-active");
+    }
+
+    this.sub("steps").children[this.value].classList.add("slider__step-active");
+  };
+
+  calcLeftByEvent(event) {
+    let newLeft =
+      (event.clientX - this.elem.getBoundingClientRect().left) /
+      this.elem.offsetWidth;
+
+    if (newLeft < 0) {
+      newLeft = 0;
+    }
+    if (newLeft > 1) {
+      newLeft = 1;
+    }
+
+    return newLeft;
+  }
+
+  onPointerUp = () => {
+    document.removeEventListener("pointermove", this.onPointerMove);
+    document.removeEventListener("pointerup", this.onPointerUp);
+
+    this.elem.classList.remove("slider_dragging");
+
+    // stick to the final value
+    this.sub("thumb").style.left = `${(this.value / this.segments) * 100}%`;
+    this.sub("progress").style.width = `${(this.value / this.segments) * 100}%`;
+
+    this.elem.dispatchEvent(
+      new CustomEvent("slider-change", {
+        detail: this.value,
+        bubbles: true,
+      })
+    );
+  };
+
+  sub(ref) {
+    return this.elem.querySelector(`.slider__${ref}`);
   }
 }
